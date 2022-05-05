50f5986b2222f8a566f0ecf90416b2d74f25472e
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-05-06 02:27:47 +0900

    Add spring boot

---
+++ mjfood/src/main/java/mj/mjfood/MjfoodApplication.java
@@ -0,0 +1,13 @@
+package mj.mjfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjfoodApplication.class, args);
+	}
+
+}
---
+++ mjfood/src/test/java/mj/mjfood/MjfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package mj.mjfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
