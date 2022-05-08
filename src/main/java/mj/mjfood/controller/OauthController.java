3328ca011fc80bf81f717f6b29f49179f26f3e35
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-05-08 18:17:36 +0900

    Test deploy

--- src/main/java/mj/mjfood/controller/OauthController.java
+++ src/main/java/mj/mjfood/controller/OauthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OauthController {
 
+    // 로그인시 리다이렉트
     @GetMapping("/auth")
     public OauthResponse jwtResponse(@RequestParam String jwt, @RequestParam Long id) {
         return new OauthResponse(id, jwt);
