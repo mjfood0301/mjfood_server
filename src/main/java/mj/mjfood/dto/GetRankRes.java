1f2daed7914f68f3adc53ec03d18ba3b2b93af03
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-06-12 18:42:13 +0900

    feat: rank 이미지 추가

--- src/main/java/mj/mjfood/dto/GetRankRes.java
+++ src/main/java/mj/mjfood/dto/GetRankRes.java
@@ -7,11 +7,13 @@ import mj.mjfood.entity.Ranks;
 public class GetRankRes {
     private Long FoodId;
     private String name;
+    private String imageUrl;
     private int count;
 
     public GetRankRes(Ranks ranks) {
         FoodId = ranks.getFood().getId();
         this.name = ranks.getFood().getName();
+        this.imageUrl = ranks.getFood().getImage();
         this.count = ranks.getCount();
     }
 }
